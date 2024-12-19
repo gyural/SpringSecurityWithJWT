d6181178fbc94cd3f7b1ff90ac6b9e5f646e8450
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-12-19 19:53:19 +0900

    feat: refresh-logic완성

--- src/main/java/com/example/springsecuritywithjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritywithjwt/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/reissue").permitAll()
                         .anyRequest().authenticated());
 
         http
